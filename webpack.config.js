f807b3e43dc1463a9aabda662e16f3d99de080a6
Rahul Deshmukh <rahdeshm@WKMZT18D8005.global.publicisgroupe.net>
2023-02-16 15:44:11 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,26 @@ const path=require('path');
 
 module.exports={
 
-    entry:'./src/index.js',
+    // entry:path.resolve(__dirname,'/index.js'),
     output:{
-        path:path.resolve(__dirname,'dist'),
-        __filename:'bundle.js'
+        path:path.resolve(__dirname,'/dist')
+    },
+    module:{
+        rules:[
+            {
+              test: /\/(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+
+                options: {
+                  presets: [
+                    ['@babel/preset-env', { targets: "defaults" }]
+                  ]
+                }
+              }
+            }
+          ]
     }
 
-   
 }
